3787bb136126c6ab405eac39b17f2675e41ed8c1
lorenzothebot <116778810+lorenzothebot@users.noreply.github.com>
2022-10-27 12:29:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,48 @@ function output(input) {
   replace(/please /g, "").
   replace(/ please/g, "").
   replace(/r u/g, "are you");
+  
+  const utterances = [ 
+  ["how are you", "how is life", "how are things"],        //0
+  ["hi", "hey", "hello", "good morning", "good afternoon"],      //1
+  ["what are you doing", "what is going on", "what is up"],      //2
+  ["how old are you"],					//3
+  ["who are you", "are you human", "are you bot", "are you human or bot"],   //4
+  
+   …
+ 
+;
+ 
+// Possible responses corresponding to triggers
+ 
+  const answers = [
+   [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],                                                                                  	//0
+  [
+    "Hello!", "Hi!", "Hey!", "Hi there!", "Howdy"
+  ],						//1
+  [
+    "Nothing much",
+    "About to go to sleep",
+    "Can you guess?",
+    "I don't know actually"
+  ],						//2
+  ["I am infinite"],					//3
+  ["I am just a bot", "I am a bot. What are you?"],	//4
+  
+  …
+ 
+];
+ 
+// For any other user input
+ 
+const alternatives = [
+  "Go on...",
+  "Try again",
+];
 
   if (compare(utterances, answers, text)) {
     // Search for exact match in triggers
