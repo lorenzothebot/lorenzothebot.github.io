beff5e882aac0b0af931ee566737e88a2a1e9269
lorenzothebot <116778810+lorenzothebot@users.noreply.github.com>
2022-10-29 12:30:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,110 +1,89 @@
-const inputField = document.getElementById("input");
-inputField.addEventListener("keydown", e => {
-  if (e.code === "Enter") {
-    let input = inputField.value;
-    inputField.value = "";
-    output(input);
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  const inputField = document.getElementById("input");
+  inputField.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+      let input = inputField.value;
+      inputField.value = "";
+      output(input);
+    }
+  });
 });
 
 function output(input) {
   let product;
-  let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
-  text = text.
-  replace(/ a /g, " ").
-  replace(/whats/g, "what is").
-  replace(/please /g, "").
-  replace(/ please/g, "").
-  replace(/r u/g, "are you");
-  
-  const utterances = [ 
-  ["how are you", "how is life", "how are things"],        //0
-  ["hi", "hey", "hello", "good morning", "good afternoon"],      //1
-  ["what are you doing", "what is going on", "what is up"],      //2
-  ["how old are you"],					//3
-  ["who are you", "are you human", "are you bot", "are you human or bot"],   //4
-  
- 
-// Possible responses corresponding to triggers
- 
-  const answers = [
-   [
-    "Fine... how are you?",
-    "Pretty well, how are you?",
-    "Fantastic, how are you?"
-  ],                                                                                  	//0
-  [
-    "Hello!", "Hi!", "Hey!", "Hi there!", "Howdy"
-  ],						//1
-  [
-    "Nothing much",
-    "About to go to sleep",
-    "Can you guess?",
-    "I don't know actually"
-  ],						//2
-  ["I am infinite"],					//3
-  ["I am just a bot", "I am a bot. What are you?"],	//4
-  
- 
-];
- 
-// For any other user input
- 
-const alternatives = [
-  "Go on...",
-  "Try again",
-];
 
-  if (compare(utterances, answers, text)) {
-    // Search for exact match in triggers
-    product = compare(utterances, answers, text);
-  } else
-  {
-    product = alternatives[Math.floor(Math.random() * alternatives.length)];
+  // Regex remove non word/space chars
+  // Trim trailing whitespce
+  // Remove digits - not sure if this is best
+  // But solves problem of entering something like 'hi1'
+
+  let text = input.toLowerCase().replace(/[^\w\s]/gi, "").replace(/[\d]/gi, "").trim();
+  text = text
+    .replace(/ a /g, " ")   // 'tell me a story' -> 'tell me story'
+    .replace(/i feel /g, "")
+    .replace(/whats/g, "what is")
+    .replace(/please /g, "")
+    .replace(/ please/g, "")
+    .replace(/r u/g, "are you");
+
+  if (compare(prompts, replies, text)) { 
+    // Search for exact match in `prompts`
+    product = compare(prompts, replies, text);
+  } else if (text.match(/thank/gi)) {
+    product = "You're welcome!"
+  } else if (text.match(/(corona|covid|virus)/gi)) {
+    // If no match, check if message contains `coronavirus`
+    product = coronavirus[Math.floor(Math.random() * coronavirus.length)];
+  } else {
+    // If all else fails: random alternative
+    product = alternative[Math.floor(Math.random() * alternative.length)];
   }
 
-  addChatEntry(input, product);
+  // Update DOM
+  addChat(input, product);
 }
 
-function compare(utterancesArray, answersArray, string) {
+function compare(promptsArray, repliesArray, string) {
   let reply;
   let replyFound = false;
-  for (let x = 0; x < utterancesArray.length; x++) {
-    for (let y = 0; y < utterancesArray[x].length; y++) {
-      if (utterancesArray[x][y] === string) {
-        let replies = answersArray[x];
+  for (let x = 0; x < promptsArray.length; x++) {
+    for (let y = 0; y < promptsArray[x].length; y++) {
+      if (promptsArray[x][y] === string) {
+        let replies = repliesArray[x];
         reply = replies[Math.floor(Math.random() * replies.length)];
         replyFound = true;
+        // Stop inner loop when input value matches prompts
         break;
       }
     }
     if (replyFound) {
+      // Stop outer loop when reply is found instead of interating through the entire array
       break;
     }
   }
   return reply;
 }
 
-function addChatEntry(input, product) {
+function addChat(input, product) {
   const messagesContainer = document.getElementById("messages");
+
   let userDiv = document.createElement("div");
   userDiv.id = "user";
   userDiv.className = "user response";
-  userDiv.innerHTML = `<span>${input}</span>`;
+  //userDiv.innerHTML = `<img src="user.png" class="avatar"><span>${input}</span>`;
   messagesContainer.appendChild(userDiv);
 
   let botDiv = document.createElement("div");
+  //let botImg = document.createElement("img");
   let botText = document.createElement("span");
   botDiv.id = "bot";
+  //botImg.src = "bot-mini.png";
+  //botImg.className = "avatar";
   botDiv.className = "bot response";
   botText.innerText = "Typing...";
   botDiv.appendChild(botText);
+  //botDiv.appendChild(botImg);
   messagesContainer.appendChild(botDiv);
+  // Keep messages at most recent
+  messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
 
-  messagesContainer.scrollTop =
-  messagesContainer.scrollHeight - messagesContainer.clientHeight;
-
-  setTimeout(() => {
-    botText.innerText = `${product}`;
-  }, 2000);
-}
