1da57089ceb4f0162028b09e71d41f0f21e7d388
lorenzothebot <116778810+lorenzothebot@users.noreply.github.com>
2022-10-30 00:02:40 -0500

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,76 +1,20 @@
 // Options the user could type in
 const prompts = [
-  ["hi", "hey", "hello", "good morning", "good afternoon"],
-  ["how are you", "how is life", "how are things"],
-  ["what are you doing", "what is going on", "what is up"],
-  ["how old are you"],
-  ["who are you", "are you human", "are you bot", "are you human or bot"],
-  ["who created you", "who made you"],
-  [
-    "your name please",
-    "your name",
-    "may i know your name",
-    "what is your name",
-    "what call yourself"
-  ],
-  ["i love you"],
-  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
-  ["bad", "bored", "tired"],
-  ["help me", "tell me story", "tell me joke"],
-  ["ah", "yes", "ok", "okay", "nice"],
-  ["bye", "good bye", "goodbye", "see you later"],
-  ["what should i eat today"],
-  ["bro"],
-  ["what", "why", "how", "where", "when"],
-  ["no","not sure","maybe","no thanks"],
-  [""],
-  ["haha","ha","lol","hehe","funny","joke"]
+  ["lorenzo"]
 ]
 
 // Possible responses, in corresponding order
 
 const replies = [
-  ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
-  [
-    "Fine... how are you?",
-    "Pretty well, how are you?",
-    "Fantastic, how are you?"
-  ],
-  [
-    "Nothing much",
-    "About to go to sleep",
-    "Can you guess?",
-    "I don't know actually"
-  ],
-  ["I am infinite"],
-  ["I am just a bot", "I am a bot. What are you?"],
-  ["The one true God, JavaScript"],
-  ["I am nameless", "I don't have a name"],
-  ["I love you too", "Me too"],
-  ["Have you ever felt bad?", "Glad to hear it"],
-  ["Why?", "Why? You shouldn't!", "Try watching TV"],
-  ["What about?", "Once upon a time..."],
-  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
-  ["Bye", "Goodbye", "See you later"],
-  ["Sushi", "Pizza"],
-  ["Bro!"],
-  ["Great question"],
-  ["That's ok","I understand","What do you want to talk about?"],
-  ["Please say something :("],
-  ["Haha!","Good one!"]
+  ["that's my name", "get a life"]
 ]
 
 // Random for any other user input
 
 const alternative = [
-  "Same",
-  "Go on...",
-  "Bro...",
-  "Try again",
-  "I'm listening...",
-  "I don't understand :/"
+  "Bro really would do that","Cope","Cope, cope, cope","Based","Bro got caught in HIGH resolution","Man got caught in 4K","You would do that","I'ma go demon time","L bozo","Bro","Bruh","Go crazy","Man went CRAZY","Bro went CRAZY","Big dubs were taken","Ez","Trippin","Man's trippin","Naaaah bro","That's incorrect","Yeah, its big brain time","small brain moment","Average human moment","Average [insert your favorite activity here] enjoyer","*laughs* You would say that *laughing intensifies*","Yeah, but have I told you that I went to nationals in sailing, bro?","Bruh moment","Bro moment","Rip bozo","Rip","Facts","True facts","Me personally; would not do that","Me personally; would'n't take that","Ima be clowning on you","Clown","On God","lol get rekt","*shifts uncomfortably* uhhhhhh *silence crescendos*","Bros literally an NPC"
 ]
 
-// Whatever else you want :)
+// Whatever else
 
-const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
+const coronavirus = ["Bro really would do that","Cope","Cope, cope, cope","Based","Bro got caught in HIGH resolution","Man got caught in 4K","You would do that","I'ma go demon time","L bozo","Bro","Bruh","Go crazy","Man went CRAZY","Bro went CRAZY","Big dubs were taken","Ez","Trippin","Man's trippin","Naaaah bro","That's incorrect","Yeah, its big brain time","small brain moment","Average human moment","Average [insert your favorite activity here] enjoyer","*laughs* You would say that *laughing intensifies*","Yeah, but have I told you that I went to nationals in sailing, bro?","Bruh moment","Bro moment","Rip bozo","Rip","Facts","True facts","Me personally; would not do that","Me personally; would'n't take that","Ima be clowning on you","Clown","On God","lol get rekt","*shifts uncomfortably* uhhhhhh *silence crescendos*","Bros literally an NPC","The King of France was Absolute, the people all were destitute, the year was 17 and 89. Churches paid no income tax, neither did aristocrats and wars were costing money all the time. But Enlightenment Ideas, had entered people’s ears like Liberty, Equality and Brotherhood. People starved and broke under feudalism's yoke knew no church or king was gonna do them any good. There was such financial crisis, the king had to take advice, to try an old thing called Estates General government. The estates general had 3 votes, Nobles, Church and Common Folks, 2 to 1 the commons lose it always went. The commons in frustration said we represent the nation, the nobles and the church must lose their privileges. And the rich were scared for real when they stormed to the Bastille and rebellions also broke out in the villages. At the palace in Versailles, the king and queen were heard to sigh when the national assembly made a bill of rights. Then the Women's march embarrassed them and chased them into Paris and the church and nobles lost their power overnight. Arguments got hot whether to keep the King or not, different groups saw different ways to run the country best. But when the Royal family tried to flee the country in disguise, they were busted and then put under a house arrest. All of Europe's Kings were terrified to hear these things, so France was threatened on all sides by enemies. And when these other kings declared the King of France had to spared, this made the King of France look worse to the Revolutionaries. They had built a new machine the quick and painless guillotine, they said it's fairer than the King's Medieval Torture rack. Then they put it on a vote and then dropped it on his throat and every king across the world just had a heart attack. They attacked from North and South, and they attacked from East and West, Revolutionary France was in a desperate state. And when the radical Marat was killed politically that was what it took for France's outlook to degenerate. The new leader Robespierre said enemies were everywhere, and for a year the Guillotine was working endlessly. And though he freed the slaves that’s true Robespierre’s head came off too and France was run by the more moderate directory. But the directory was lame and France's only good news came from their armies now in Europe winning more and more. And the man who made them win was General Napoleon and when he came back home he made himself the emperor. So it was a painful climb to get from Kings to Modern times, but the French Revolution always paved the way. 'Cause the ideas that sparked the first, have never ever been reversed, it was biggest change that shaped our western world today"]
