6fa2ebd63d5ec4a9b8355d2919d45ec7204c5f66
lorenzothebot <116778810+lorenzothebot@users.noreply.github.com>
2022-10-30 12:34:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -70,19 +70,19 @@ function addChat(input, product) {
   let userDiv = document.createElement("div");
   userDiv.id = "user";
   userDiv.className = "user response";
-  userDiv.innerHTML = `<span>${input}</span>`;
+  userDiv.innerHTML = `<img src="user.png" class="avatar"><span>${input}</span>`;
   messagesContainer.appendChild(userDiv);
 
   let botDiv = document.createElement("div");
-  //let botImg = document.createElement("img");
+  let botImg = document.createElement("img");
   let botText = document.createElement("span");
   botDiv.id = "bot";
-  //botImg.src = "Lorenzo.jpg";
-  //botImg.className = "avatar";
+  botImg.src = "Lorenzo.jpg";
+  botImg.className = "avatar";
   botDiv.className = "bot response";
   botText.innerText = "Thinking...";
+  botDiv.appendChild(botImg);
   botDiv.appendChild(botText);
-  //botDiv.appendChild(botImg);
   messagesContainer.appendChild(botDiv);
   // Keep messages at most recent
   messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
